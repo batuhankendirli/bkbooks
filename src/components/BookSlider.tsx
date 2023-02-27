307550f9d4c2051c624366a053aa9cdebfc17e56
batuhankendirli <batuhan.kendirli@viravira.co>
2023-02-27 23:47:08 +0300

    categories section partially completed

---
+++ src/components/BookSlider.tsx
@@ -0,0 +1,50 @@
+import BookCard from './BookCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+import { FreeMode } from 'swiper';
+
+const BookSlider = ({ category }: { category: string }) => {
+  return (
+    <div className="book-slider">
+      <h1 className="book-slider-header">{category}</h1>
+      <Swiper
+        spaceBetween={10}
+        modules={[FreeMode]}
+        slidesPerView="auto"
+        freeMode={true}
+        touchEventsTarget="container"
+        style={{
+          padding: '1.2rem .8rem',
+        }}
+      >
+        {Array(10)
+          .fill(0)
+          .map((_, index) => {
+            return (
+              <SwiperSlide className="book-slider-slide" key={index}>
+                <BookCard
+                  data={{
+                    id: String(index),
+                    volumeInfo: {
+                      authors: ['Batuhan Kendirli', 'Batuhan Kendirli'],
+                      publisher: 'Something',
+                      title: 'Test test test',
+                      imageLinks: {
+                        smallThumbnail:
+                          'https://publications.iarc.fr/uploads/media/default/0001/02/thumb_1203_default_publication.jpeg',
+                      },
+                    },
+                  }}
+                />
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default BookSlider;
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,5 +1,21 @@
+import BookSlider from '../components/BookSlider';
+
 const Categories = () => {
-  return <div className="section-mt">Categories</div>;
+  return (
+    <div className="section-categories section-mt">
+      <p className="section-categories-header">
+        Don't know what to read? Here are some cool categories listed for you!
+      </p>
+      <div className="section-categories-slides">
+        <BookSlider category="Korku" />
+        <BookSlider category="Aksiyon" />
+        <BookSlider category="Bilim & Kurgu" />
+        <BookSlider category="Gerilim" />
+        <BookSlider category="Biyografi" />
+        <BookSlider category="Sosyoloji" />
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
